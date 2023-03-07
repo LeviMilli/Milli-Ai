66fd16deb4331544724fb2299cf9cf3c511633fc
Levi Milli <levi.millikin@gmail.com>
2023-03-06 16:02:04 -0800

    more typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,52 @@
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Navbar from '@/components/Navbar'
-import Chatbot from '@/components/Chatbot'
-import Searchlist from '@/components/Searchlist'
-import Search from '@/models/Search'
-import { useState, useContext, useEffect } from 'react'
-import { AppContext } from "../context/context";
-import ImageGen from '@/components/ImageGen'
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+import Navbar from '@/components/Navbar';
+import Chatbot from '@/components/Chatbot';
+import Searchlist from '@/components/Searchlist';
+import Search from '@/models/Search';
+import { useState, useContext, useEffect, createContext } from 'react';
+import ImageGen from '@/components/ImageGen';
 
+const inter = Inter({ subsets: ['latin'] });
 
-const inter = Inter({ subsets: ['latin'] })
+interface Props {
+  searchList: {
+    _id: string;
+    text: string;
+    answer: string;
+    likes: number;
+  }[];
+}
 
-export async function getServerSideProps() {
+interface AppContextValue {
+  list: {
+    _id: string;
+    text: string;
+    answer: string;
+    likes: number;
+  }[];
+  setList: React.Dispatch<
+    React.SetStateAction<{
+      _id: string;
+      text: string;
+      answer: string;
+      likes: number;
+    }[]>
+  >;
+}
+
+export const AppContext = createContext<AppContextValue | undefined>(undefined);
+
+export async function getServerSideProps(): Promise<{ props: Props }> {
   try {
     const data = await Search.find({}).lean();
-    console.log(data)
+    console.log(data);
 
     const searchList = data.map(({ _id, text, answer, likes }) => ({
       _id: _id.toString(),
       text,
       answer,
-      likes
+      likes,
     }));
 
     return {
@@ -39,21 +65,21 @@ export async function getServerSideProps() {
   }
 }
 
-export default function Home({ searchList }) {
-  const { list, setList } = useContext(AppContext)
+export default function Home({ searchList }: Props): JSX.Element {
+  const appContext = useContext(AppContext);
 
   useEffect(() => {
-    setList(searchList)
-  }, [searchList, setList])
+    if (appContext) {
+      appContext.setList(searchList);
+    }
+  }, [searchList, appContext]);
 
   return (
     <>
-      <Navbar/>
-      <Chatbot/>
-      <ImageGen/>
-      <Searchlist list={list} />
+      <Navbar />
+      <Chatbot />
+      <ImageGen />
+      <Searchlist list={appContext?.list || []} />
     </>
-  )
+  );
 }
-
-
