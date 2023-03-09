ee574413bc05625222cefa117ea77e89117161d8
Levi Milli <levi.millikin@gmail.com>
2023-03-08 16:45:50 -0800

    Revert "more typescript"
    This reverts commit 66fd16deb4331544724fb2299cf9cf3c511633fc.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,26 @@
-import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
-import Navbar from '@/components/Navbar';
-import Chatbot from '@/components/Chatbot';
-import Searchlist from '@/components/Searchlist';
-import Search from '@/models/Search';
-import { useState, useContext, useEffect, createContext } from 'react';
-import ImageGen from '@/components/ImageGen';
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Navbar from '@/components/Navbar'
+import Chatbot from '@/components/Chatbot'
+import Searchlist from '@/components/Searchlist'
+import Search from '@/models/Search'
+import { useState, useContext, useEffect } from 'react'
+import { AppContext } from "../context/context";
+import ImageGen from '@/components/ImageGen'
 
-const inter = Inter({ subsets: ['latin'] });
 
-interface Props {
-  searchList: {
-    _id: string;
-    text: string;
-    answer: string;
-    likes: number;
-  }[];
-}
+const inter = Inter({ subsets: ['latin'] })
 
-interface AppContextValue {
-  list: {
-    _id: string;
-    text: string;
-    answer: string;
-    likes: number;
-  }[];
-  setList: React.Dispatch<
-    React.SetStateAction<{
-      _id: string;
-      text: string;
-      answer: string;
-      likes: number;
-    }[]>
-  >;
-}
-
-export const AppContext = createContext<AppContextValue | undefined>(undefined);
-
-export async function getServerSideProps(): Promise<{ props: Props }> {
+export async function getServerSideProps() {
   try {
     const data = await Search.find({}).lean();
-    console.log(data);
+    console.log(data)
 
     const searchList = data.map(({ _id, text, answer, likes }) => ({
       _id: _id.toString(),
       text,
       answer,
-      likes,
+      likes
     }));
 
     return {
@@ -65,21 +39,21 @@ export async function getServerSideProps(): Promise<{ props: Props }> {
   }
 }
 
-export default function Home({ searchList }: Props): JSX.Element {
-  const appContext = useContext(AppContext);
+export default function Home({ searchList }) {
+  const { list, setList } = useContext(AppContext)
 
   useEffect(() => {
-    if (appContext) {
-      appContext.setList(searchList);
-    }
-  }, [searchList, appContext]);
+    setList(searchList)
+  }, [searchList, setList])
 
   return (
     <>
-      <Navbar />
-      <Chatbot />
-      <ImageGen />
-      <Searchlist serverList={searchList} />
+      <Navbar/>
+      <Chatbot/>
+      <ImageGen/>
+      <Searchlist list={list} />
     </>
-  );
+  )
 }
+
+
